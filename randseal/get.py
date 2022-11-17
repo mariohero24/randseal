f01a53fe9fa399574f99db8a9c4617f983eca7ee
Guard Boi <105892653+mariohero24@users.noreply.github.com>
2022-11-17 09:21:36 -0800

    ...

--- randseal/get.py
+++ randseal/get.py
@@ -2,14 +2,13 @@ import discord, random
 from importlib import resources
 
 def seal():
-	"""
-	Returns a `discord.File()` of a seal!
+	f"""
+	Returns a `discord.File()` of a seal for py-cord
 	"""
 	sealrand = f"{random.randrange(1, 82)}"
 	if len(sealrand) == 1:
 		sussy = sealrand
 		sealrand = "0" + f"{sussy}"
-	with resources.open_binary('randseal', f'00{sealrand}.jpg') as fp:
-		img = fp.read()
-	file = discord.File(fp=img, filename=f"{sealrand}.png")
-	return file
+	with resources.open_text('randseal', f'00{sealrand}.jpg') as fp:
+		file = discord.File(fp=fp.name, filename=f"{sealrand}.png")
+		return file
