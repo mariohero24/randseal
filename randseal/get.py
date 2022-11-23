55d597ff5e2edd5cc36f3f95dd3ea34d9ef8c646
Guard Boi <105892653+mariohero24@users.noreply.github.com>
2022-11-23 09:35:09 -0800

    v1.3.0

--- randseal/get.py
+++ randseal/get.py
@@ -1,7 +1,7 @@
 import discord, random
 from importlib import resources
 
-def seal(num: int=None):
+def file(num: int=None):
 	"""
 	Returns a `discord.File()` of a seal for py-cord
 	"""
@@ -16,5 +16,21 @@ def seal(num: int=None):
 		sussy = sealrand
 		sealrand = "0" + f"{sussy}"
 	with resources.open_text('randseal', f'00{sealrand}.jpg') as fp:
-		file = discord.File(fp=fp.name, filename=f"{sealrand}.png")
-		return file
+		return discord.File(fp=fp.name, filename=f"{sealrand}.png")
+
+def embed(num: int=None, title: str="** **"):
+	"""
+	Returns a `discord.Embed()` of a seal
+	"""
+	if num == None:
+		sealrand = f"{random.randrange(1, 82)}"
+	else: 
+		if not sealrand > 82:
+			sealrand = num
+		else:
+			sealrand = f"{random.randrange(1, 82)}"
+	if len(sealrand) == 1:
+		sussy = sealrand
+		sealrand = "0" + f"{sussy}"
+	embeda = discord.Embed(colour=0x2f3136, title=title).set_image(url=f"https://raw.githubusercontent.com/mariohero24/randseal/main/randseal/00{sealrand}.jpg")
+	return embeda
