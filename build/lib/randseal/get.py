fbba6657532d0b6db21c91986843a08a7ab19f26
Guard Boi <105892653+mariohero24@users.noreply.github.com>
2022-11-30 12:28:07 -0800

    v1.3.5

--- build/lib/randseal/get.py
+++ build/lib/randseal/get.py
@@ -6,36 +6,40 @@ def file(num: int=None):
 	"""
 	Returns a `discord.File()` of a seal for py-cord
 	"""
-	if num == None:
-		sealrand = f"{random.randrange(1, 82)}"
-	else: 
-		if not sealrand > 82:
-			sealrand = num
-		else:
+	try:
+		if num == None:
 			sealrand = f"{random.randrange(1, 82)}"
-	if len(sealrand) == 1:
-		sussy = sealrand
-		sealrand = "0" + f"{sussy}"
-	with resources.open_text('randseal', f'00{sealrand}.jpg') as f:
-		return discord.File(fp=f.name, filename=f"{sealrand}.png")
+		else: 
+			if not sealrand > 82:
+				sealrand = num
+			else:
+				sealrand = f"{random.randrange(1, 82)}"
+		if len(sealrand) == 1:
+			sussy = sealrand
+			sealrand = "0" + f"{sussy}"
+	finally:
+		with resources.open_text('randseal', f'00{sealrand}.jpg') as f:
+			return discord.File(fp=f.name, filename=f"{sealrand}.png")
 
 randoms = ("Here is your seal!", "Arff Arff!", ":3")
 
-def embed(num: int=None, title: str=random.choice(randoms)):
+def embed(num: int = ..., title: str = random.choice(randoms)):
 	"""
 	Returns a `discord.Embed()` of a seal
 	"""
-	if num == None:
-		sealrand = f"{random.randrange(1, 82)}"
-	else: 
-		if not sealrand > 82:
-			sealrand = num
-		else:
+	try:
+		if num == None:
 			sealrand = f"{random.randrange(1, 82)}"
-	if len(sealrand) == 1:
-		sussy = sealrand
-		sealrand = "0" + f"{sussy}"
-	embeda = discord.Embed(colour=blank(), title=title).set_image(url=f"https://raw.githubusercontent.com/mariohero24/randseal/main/randseal/00{sealrand}.jpg")
-	return embeda
+		else: 
+			if not sealrand > 82:
+				sealrand = num
+			else:
+				sealrand = f"{random.randrange(1, 82)}"
+		if len(sealrand) == 1:
+			sussy = sealrand
+			sealrand = "0" + f"{sussy}"
+	finally:
+		embeda = discord.Embed(colour=blank(), title=title).set_image(url=f"https://raw.githubusercontent.com/mariohero24/randseal/main/randseal/00{sealrand}.jpg")
+		return embeda
 
 # python3 -m twine upload --repository pypi dist/*
