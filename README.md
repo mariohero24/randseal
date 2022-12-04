fa69c536f5fa7dc4951464920f80b14a83fe14d5
Guard Boi <105892653+mariohero24@users.noreply.github.com>
2022-12-03 16:29:53 -0800

    v1.4.2

--- README.md
+++ README.md
@@ -1,21 +1,20 @@
 # randseal
 Simple package that produces a seal image. The image is then output as a `discord.File` for Pycord.
 
-### Usage examples
+### Usage example
 ```py
-import randseal
-from discord import Bot, Embed
+from randseal import get
+from discord import Bot, Intents
 
-bot = Bot(intents=discord.Intents.default())
+bot = Bot(intents=Intents.default())
 
 @bot.slash_command()
 async def sealimg(ctx):
-	await ctx.respond(file=ranseal.get.file())
+	await ctx.respond(file=get.file())
 
 @bot.slash_command()
-async def sealembed(ctx, title):
-	embed = discord.Embed(colour=randseal.utils.blank(), title=title)
-	await ctx.respond(embed=ranseal.get.embed(embed=embed))
+async def sealembed(ctx):
+	await ctx.respond(embed=get.embed())
 
 bot.run("token")
 ```
--- randseal/get.py
+++ randseal/get.py
@@ -1,60 +1,37 @@
-from random import randrange
+import discord, random
 from importlib import resources
-from discord import File, Embed
+from .utils import blank
 
 def file():
 	"""
-	Ouputs a `discord.File` of a seal.
-	## Returns
-	### `discord.File`
+	Returns a `discord.File()` of a seal for py-cord
 	"""
 	try:
 		try:
-			sealrand = f"{randrange(1, 82)}"
+			sealrand = f"{random.randrange(1, 82)}"
 		finally:
 			if len(sealrand) == 1:
-				try:
+				sussy = sealrand
+				sealrand = "0" + f"{sussy}"
+	finally:
+		with resources.open_text('randseal', f'00{sealrand}.jpg') as f:
+			return discord.File(fp=f.name, filename=f"{sealrand}.png")
+
+def embed(title: str = random.choice(["Here is your seal!", "Arff Arff!", ":3", "Seap!"])):
+	"""
+	Returns a `discord.Embed()` of a seal
+	"""
+	try:
+		try:
+			sealrand = f"{random.randrange(1, 82)}"
+		finally:
+			if len(sealrand) == 1:
 					sussy = sealrand
-				finally:
 					sealrand = "0" + f"{sussy}"
 	finally:
 		try:
-			f = resources.open_text('randseal', f'00{sealrand}.jpg')
+			embeda = discord.Embed(colour=blank(), title=title).set_image(url=f"https://raw.githubusercontent.com/mariohero24/randseal/main/randseal/00{sealrand}.jpg")
 		finally:
-			try:
-				return File(fp=f.name, filename=f"{sealrand}.png")
-			finally:
-				f.close()
-
-def embed(embed: Embed):
-	"""
-	Ouputs a modifed `discord.Embed`
-	## Parameters
-	### embed
-	A `discord.Embed` that will be modified by the function, setting the image to a seal.
-	## Returns
-	#### `discord.Embed`
-	"""
-	try:
-		try:
-			sealrand = str({randrange(1, 83)})
-		finally:
-			try:
-				if sealrand == "7":
-					sealrand = "83"
-			finally:
-				if len(sealrand) == 1:
-					try:
-						sussy = sealrand
-					finally:
-						sealrand = "0" + f"{sussy}"
-	finally:
-		try:
-			embeda = embed
-		finally:
-			try:
-				embeda.set_image(url=f"https://raw.githubusercontent.com/mariohero24/randseal/main/randseal/00{sealrand}.jpg")
-			finally:
-				return embeda
+			return embeda
 
 # python3 -m twine upload --repository pypi dist/*
